373ad2f161dcb24c3e062f6395178c32956b8aa6
methodmain <methodmain@yahoo.com>
2018-03-05 05:32:23 +0100

    Set up google maps

--- src/routes/Home/components/Home.js
+++ src/routes/Home/components/Home.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import { Container } from 'native-base';
+import MapContainer from './MapContainer';
+
 class Home extends Component {
   componentDidMount() {
     this.props.setName();
   }
 
   render() {
-    console.log('Displays components Home');
-    return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ color: '#000000' }}>Helllo {this.props.name} </Text>
-    </View>);
+    const region = {
+      latitude: 6.500501,
+      longitude: 3.366604,
+      latitudeDelta: 0.0022,
+      longitudeDelta: 0.0421
+    };
+
+    return (
+      <Container>
+        <MapContainer region={region} />
+      </Container>
+  );
   }
 }
 
